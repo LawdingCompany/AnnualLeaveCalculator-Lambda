5b062655d9ac56b2e4513d11a8c803f5ec86be08
김재윤 <jaeyun1723@naver.com>
2025-05-18 20:36:56 +0900

    fix : 로그 출력 객체 어노테이션 추가 #4
    - request, context, result 객체 @ToString 어노테이션 추가

--- leavecalc/src/main/java/com/lawding/leavecalc/dto/AnnualLeaveRequest.java
+++ leavecalc/src/main/java/com/lawding/leavecalc/dto/AnnualLeaveRequest.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class AnnualLeaveRequest {
     @JsonProperty("calculationType")
     private int calculationType;
